f9abefef013a02677dff102df1fa33034addf0e8
mmalikli <muradmalikli.edu@mail.ru>
2023-08-06 20:48:49 +0400

    Docker-compose file added

--- api.go
+++ api.go
@@ -9,45 +9,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
-
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-type ApiError struct {
-	Error string
-}
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			// handle the error 
-			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-		}
-	}
-}
-
 type APIserver struct {
 	listenAddr string
+	store Storage
+}
+
+func NewAPIServer(listenAddr string, store Storage) *APIserver {
+	return &APIserver{
+		listenAddr: listenAddr,
+		store: store,
+	}
 }
 
 func (s *APIserver) Run() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+  router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
 
 	log.Println("JSON API server running on port: ", s.listenAddr)
-	http.ListenAndServe(s.listenAddr, router)
-}
-
-func NewAPIServer(listenAddr string) *APIserver {
-	return &APIserver{
-		listenAddr: listenAddr,
+	if err := http.ListenAndServe(s.listenAddr, router); err != nil {
+		log.Fatal("Listen and Serve: ", err)
 	}
 }
+
 
 func (s *APIserver) handleAccount(w http.ResponseWriter, r *http.Request) error {
 	//return nil
@@ -75,7 +60,9 @@ func (s *APIserver) handleAccount(w http.ResponseWriter, r *http.Request) error 
 	
 }
 func (s *APIserver) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+//	id := mux.Vars(r)["id"]
+	fmt.Println(mux.Vars(r))
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 func (s *APIserver) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
@@ -88,3 +75,24 @@ func (s *APIserver) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 // func (s *APIserver) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 // 	return nil
 // }
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle the error 
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
---
+++ storage.go
@@ -0,0 +1,16 @@
+package main
+
+type Storage interface {
+	CreateAccount(*Account) error
+	DeleteAccount(*Account) error
+	UpdateAccount(*Account) error
+	GetAccountByID(int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	
+}
